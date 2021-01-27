d64b50ff41ab497ffe0faaa6ce7c6d93fbf53a05
diegun_99 <diegun123456789@gmail.com>
2021-01-27 17:54:59 -0500

    comandos http

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const  express = require('express');
 const app = express();
 
+
 app.get('/',(req,res) => {
-    res.send('holi mundo');
+    res.send('Peticion get recibido');
 });
 
+app.post('/about',(req,res)=>{
+    res.send('post request recibido');
+});
 
+app.put('/about',(req,res)=>{
+    res.send('update request recibido');
+});
+
+app.delete('/about',(req,res)=>{
+    res.send('<h1>Delete request<h1>');
+});
 
 
 app.listen(3000, () => {
